6ba897497cd3adc782f894664d9c0672ff97bd3b
IllegalTempo <jedtsui0704@gmail.com>
2025-03-11 01:24:15 +0000

    Fix

--- Assets/Scripts/Network/GameClient.cs
+++ Assets/Scripts/Network/GameClient.cs
@@ -30,7 +30,7 @@ public class GameClient : ConnectionManager
             { (int)PacketSend.ServerPackets.RoomInfoOnPlayerEnterRoom,PacketHandles_Method.Client_Handle_InitRoomInfo },
             { (int)PacketSend.ServerPackets.UpdatePlayerEnterRoomForExistingPlayer,PacketHandles_Method.Client_Handle_NewPlayerJoin },
             { (int)PacketSend.ServerPackets.PlayerQuit,PacketHandles_Method.Client_Handle_PlayerQuit },
-
+            { (int)PacketSend.ServerPackets.DistributeMovement,PacketHandles_Method.Client_Handle_ReceivedPlayerMovement}
 
         };
     }
